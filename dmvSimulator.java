fcc39b14035e3878b55357e37d75ace1bf470bab
JeremyGerm <jeremyrwiening@gmail.com>
2024-02-28 20:46:32 -0500

    welcome msg

--- dmvSimulator.java
+++ dmvSimulator.java
@@ -3,3 +3,13 @@
  * CSC109
  * DMV Simulation
  */
+
+import java.util.Random;
+
+public class dmvSimulator{
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the DMV!");
+    }
+}
