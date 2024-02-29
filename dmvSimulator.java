87a184a5e17b8c04d73a508732c8da1490c00fcc
JeremyGerm <jeremyrwiening@gmail.com>
2024-02-28 21:07:01 -0500

    Created the setup-sim

--- dmvSimulator.java
+++ dmvSimulator.java
@@ -8,8 +8,29 @@ import java.util.Random;
 
 public class dmvSimulator{
 
+    private static int randomNumber() {
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the DMV!");
+
+        int userNumber = randomNumber();
+        System.out.println("Your number is: " + userNumber + ". Please wait until your number is called.");
+
+        int currentNumber = userNumber + 1; // Start from the number 1 greater than users number
+
+        while (currentNumber != userNumber) {
+            System.out.println("Number " + currentNumber + " please come to the counter.");
+            currentNumber++; //Increment currentnumber (+1 each time around the loop)
+            if (currentNumber > 100) {
+                currentNumber = 1;
+            }
+        }
+
+        System.out.println("Number " + userNumber + ", you're up. Please come to the counter.");
+        System.out.println("Sorry, you don't have the required paperwork.\nPlease come back with all the proper paper work.");
     }
 }
